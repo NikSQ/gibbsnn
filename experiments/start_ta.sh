eb7fa9a78d24335b219a0defde434a2659393258
Nikolaus Kopp <kopp13@dsplab02.spsc.tugraz.at>
2018-05-14 15:07:12 +0200

    -

--- experiments/start_ta.sh
+++ experiments/start_ta.sh
@@ -11,7 +11,7 @@
 #SBATCH --gres=gpu
 #SBATCH --partition=gpu,gpu2,gpu6
 #SBATCH --exclude=diannao
-#SBATCH --array=0-7
+#SBATCH --array=0-3
 
 #################
 # configuration #
--- experiments/ternary_activations.py
+++ experiments/ternary_activations.py
@@ -7,30 +7,31 @@ from src.activation import get_activation_function
 from src.run_experiment import run_experiment
 
 # TODO: job name should include the actual index of the job
-#task_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
+task_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
 
-task_id = 0
 path = '../results/ternary_act/job_' + str(task_id)
 
-run_config = {'n_epochs': 4,
-              'block_size': 2,
+run_config = {'n_epochs': 7,
+              'block_size': 8,
               'store_acts': True,
-              'store_acts_every': 1,
+              'store_acts_every': 3,
               'store_vars': True,
-              'store_vars_every': 1,
+              'store_vars_every': 3,
               'path': path}
 
-act_func = get_activation_function('extended_ternary_sign')
-act_func.set_params([1 + 2 * task_id])
-act_funcs = [act_func, act_func]
+act_func1 = get_activation_function('extended_ternary_sign')
+act_func2 = get_activation_function('extended_ternary_sign')
+act_func1.set_params([5 + 8 * task_id])
+act_func2.set_params([3 + 4 * task_id])
+act_funcs = [act_func1, act_func2]
 
 
-config = {'layout': [8, 8],
+config = {'layout': [370, 350],
           'weight_type': 'binary',
           'act_funcs': act_funcs,
           'bias_vals': [None, None, None],
-          'keep_probs': [1.0, 1.0],
-          'flat_factor': [1.0, 1.0, 1.0],
+          'keep_probs': [0.8, 0.8],
+          'flat_factor': [1.2, 1.2, 1.2],
           'sampling_sequence': 'stochastic'}
 
 run_experiment(run_config, config, 'mnist_basic')
