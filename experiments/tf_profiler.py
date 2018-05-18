d281b1d095fee79bf7a7b84560a2af3ef22ece94
Nik <nkopp@student.tugraz.at>
2018-05-18 19:21:16 +0200

    .

--- experiments/tf_profiler.py
+++ experiments/tf_profiler.py
@@ -22,11 +22,9 @@ act_func.set_params([])
 act_funcs = [act_func, act_func]
 
 x_tr, y_tr, x_va, y_va, x_te, y_te = load_dataset('mnist_basic')
-x_tr = x_tr[:, :]
-x_va = x_va[:, :]
 
 config = {'layout': [x_tr.shape[1], 200, 200, y_tr.shape[1]],
-          'weight_type': 'ternary',
+          'weight_type': 'binary',
           'act_funcs': act_funcs,
           'bias_vals': [None, None, None],
           'keep_probs': [1.0, 1.0],
