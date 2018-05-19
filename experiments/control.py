4e9058e06549581594acc583be9999566ffc5c81
Nikolaus Kopp <kopp13@dsplab02.spsc.tugraz.at>
2018-05-19 13:01:19 +0200

    .

--- experiments/control.py
+++ experiments/control.py
@@ -7,30 +7,31 @@ from src.activation import get_activation_function
 from src.run_experiment import run_experiment
 
 task_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
-#task_id = 0
-path = '../results/control/'
+path = '../results/control/job_' + str(task_id) + '/'
 
-run_config = {'n_epochs': 15,
+run_config = {'n_epochs': 10,
               'block_size': 5,
               'store_acts': True,
-              'store_acts_every': 1,
+              'store_acts_every': 5,
               'store_vars': True,
-              'store_vars_every': 1,
+              'store_vars_every': 5,
               'path': path}
 
-act_func1 = get_activation_function('stair')
-act_func2 = get_activation_function('stair')
+act_func1 = get_activation_function('binary_sign')
+act_func2 = get_activation_function('binary_sign')
 
-act_func1.set_params([5 + task_id / 2, 4])
-act_func2.set_params([2 + task_id % 2, 4])
+act_func1.set_params([])
+act_func2.set_params([])
 act_funcs = [act_func1, act_func2]
+layer_1 = 180 + 40 * int(task_id / 2)
+layer_2 = 180 + 40 * int(task_id % 2)
 
 
-config = {'layout': [120, 90],
+config = {'layout': [layer_1, layer_2],
           'weight_type': 'ternary',
           'act_funcs': act_funcs,
           'bias_vals': [None, None, None],
-          'keep_probs': [0.9, 0.9],
+          'keep_probs': [1., 1.],
           'flat_factor': [1., 1., 1.],
           'sampling_sequence': 'stochastic'}
 
--- experiments/tf_profiler.py
+++ experiments/tf_profiler.py
@@ -23,7 +23,7 @@ act_funcs = [act_func, act_func]
 
 x_tr, y_tr, x_va, y_va, x_te, y_te = load_dataset('mnist_basic')
 
-config = {'layout': [x_tr.shape[1], 200, 200, y_tr.shape[1]],
+config = {'layout': [x_tr.shape[1], 20, 20, y_tr.shape[1]],
           'weight_type': 'binary',
           'act_funcs': act_funcs,
           'bias_vals': [None, None, None],
