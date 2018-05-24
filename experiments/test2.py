fde0384c557e664fc98a487f33da190648fb2ab8
Nikolaus Kopp <kopp13@dsplab02.spsc.tugraz.at>
2018-05-24 14:57:43 +0200

    .

--- experiments/test2.py
+++ experiments/test2.py
@@ -9,24 +9,26 @@ from src.run_experiment import run_experiment
 task_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
 path = '../results/test2/job_' + str(task_id) + '/'
 
-run_config = {'n_epochs': 22,
+run_config = {'n_epochs': 20,
               'block_size': 4,
               'store_acts': True,
-              'store_acts_every': 7,
+              'store_acts_every': 1,
               'store_vars': True,
-              'store_vars_every': 7,
+              'store_vars_every': 1,
               'path': path}
 
-act_func1 = get_activation_function('stair')
-act_func2 = get_activation_function('stair')
+act_func1 = get_activation_function('binary_sign')
+act_func2 = get_activation_function('binary_sign')
 
-act_func1.set_params([4, 4])
-act_func2.set_params([4, 4])
+act_func1.set_params([])
+act_func2.set_params([])
 act_funcs = [act_func1, act_func2]
-layer_1 = 250 
-layer_2 = 250
-keep_probs1 = 0.92 ** int(task_id / 3 + 1)
-keep_probs2 = 0.92 ** int(task_id % 3 + 1)
+layer_1 = 180 
+layer_2 = 180
+#keep_probs1 = 0.92 ** int(task_id / 3 + 1)
+#keep_probs2 = 0.92 ** int(task_id % 3 + 1)
+keep_probs1 = 1.0
+keep_probs2 = 1.0
 
 
 config = {'layout': [layer_1, layer_2],
